79d9bc64678960f0f72a21c523574e9e5e1737fa
Ridjali Hassani <hassani9@live.fr>
2025-02-24 00:58:30 +0100

    first commit_1.0

--- app/main.js
+++ app/main.js
@@ -85,12 +85,48 @@ const map = new Map({
 });
 
 
+// ========================================================================================
+// ==== Définition de la fonction des ckeckbox pour afficher et masquer les couche  ======= 
+// ========================================================================================
+
+// Fonctions pour afficher et masquer la couche country 
+const checkbox_pt_justice = document.getElementById('checkbox-pt_justice');
+checkbox_pt_justice.addEventListener('change', (event) => {
+  if (event.currentTarget.checked) {
+    // On fait des trucs quand la checkbox est checkée
+    pt_justice.setVisible(true);
+    console.log('test');
+  } else {
+    // On fait des trucs quand la checkbox n’est PAS checkée
+    pt_justice.setVisible(false);
+  }
+});
+
+
+document.querySelectorAll('input[name="checkbox-group"]').forEach((radio) => {
+  radio.addEventListener("change", (event) => {
+    if (event.target.id === "checkbox-cour_appel") {
+      cour_appel.setVisible(true);
+      trib_judiciaire.setVisible(false);
+      prudhomme.setVisible(false);
+    } else if (event.target.id === "checkbox-trib_judiciaire") {
+      trib_judiciaire.setVisible(true);
+      cour_appel.setVisible(false);
+      prudhomme.setVisible(false);
+    } else if (event.target.id === "checkbox-prudhomme") {
+      prudhomme.setVisible(true);
+      cour_appel.setVisible(false);
+      trib_judiciaire.setVisible(false);
+    }
+  });
+});
 
 
 // ========================================================================================
 // ===============================  Fonctions accésoires  =============================== 
 // ========================================================================================
 
+// Bouton d'affichage et masquage de l'onglet gestion des couches
 document.getElementById('btn-layer-panel').addEventListener('click', function() {
   var contentPanel = document.getElementById('content-layer-panel');
   if (contentPanel.style.display === 'none' || contentPanel.style.display === '') {
@@ -103,3 +139,52 @@ document.getElementById('btn-layer-panel').addEventListener('click', function() 
 document.getElementById('btn-close-layer-panel').addEventListener('click', function() {
   document.getElementById('content-layer-panel').style.display = 'none';
 });
+
+
+// Bouton d'affichage et masquage de l'onglet info
+const toggleButton = document.getElementById('toggle-size-button');
+const ongletInfo = document.getElementById('info-panel-overlay');
+const toggleImg = document.getElementById('toggle-img');
+
+// Initialiser l'image en fonction de l'état par défaut
+if (ongletInfo.classList.contains('reduit')) {
+  toggleImg.src = 'dist/assets/img/prev.png'; // Image pour l'état réduit
+} else {
+  toggleImg.src = 'dist/assets/img/next.png'; // Image pour l'état normal
+}
+
+toggleButton.addEventListener('click', () => {
+  if (ongletInfo.style.display === 'none' || ongletInfo.style.display === '') {
+    ongletInfo.style.display = 'block';
+    toggleImg.src = 'dist/assets/img/prev.png'; // Image pour l'état affiché
+  } else {
+    ongletInfo.style.display = 'none';
+    toggleImg.src = 'dist/assets/img/next.png'; // Image pour l'état masqué
+  }
+});
+
+
+function toggleDialog(buttonId, dialogId, otherDialogIds) {
+  document.getElementById(buttonId).addEventListener('click', function() {
+      const dialog = document.getElementById(dialogId);
+      const otherDialogs = otherDialogIds.map(id => document.getElementById(id));
+      
+      // Fermer les autres dialogues s'ils sont ouverts
+      otherDialogs.forEach(otherDialog => {
+          if (otherDialog.open) {
+              otherDialog.close();
+          }
+      });
+      
+      // Ouvrir ou fermer le dialogue actuel
+      if (dialog.open) {
+      } else {
+          dialog.show(); 
+      }
+  });
+}
+
+// Utilisation de la fonction générique pour les boutons
+toggleDialog('button-text', 'text-info', ['stat-info', 'stat-list']);
+toggleDialog('button-stat', 'stat-info', ['text-info', 'stat-list']);
+toggleDialog('button-list', 'stat-list', ['text-info', 'stat-info']);
